f5f0983b752c4b2d38d4e57d8d24192937f81bb2
krishna9886 <krishna.22060@stu.upes.ac.in>
2026-02-10 21:20:52 +0530

    Add files via upload

--- q9.c
+++ q9.c
@@ -11,7 +11,7 @@ int main(){
     for(int i=0;i<len/2;i++){
             char temp = str[i];
             str[i] = str[len-1-i];
-        str[len-1-i] = temp;
+            str[len-1-i] = temp;
     }
     printf("%s\n",str);
     return 0;
