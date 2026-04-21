8738db7aa27aaf61c0b24dcc71b1bdfe952fa31e
krishna aggarwal <krishna.22060@stu.upes.ac.in>
2026-04-21 10:23:18 +0530

    Add day80 - Floyd-Warshall Algorithm (All-Pairs Shortest Path)

---
+++ day80.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define INF 99999
+#define V 4
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    // Initialize the solution matrix same as input graph matrix
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Add all vertices one by one to the set of intermediate vertices
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF
+                    && dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the result
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("-1");
+            else
+                printf("%d", dist[i][j]);
+            if (j < V - 1) printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 5, INF, 10},
+        {INF, 0, 3, INF},
+        {INF, INF, 0, 1},
+        {INF, INF, INF, 0}
+    };
+    floydWarshall(graph);
+    return 0;
+}
