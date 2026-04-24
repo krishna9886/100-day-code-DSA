fc0769cd3426c1ca43a0b8a142bc8e3366a199b2
krishna aggarwal <krishna.22060@stu.upes.ac.in>
2026-04-24 16:35:15 +0530

    Add day81 - Bubble Sort

---
+++ day81.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// Day 81 - Question 1: Implement Bubble Sort
+// Topic: Sorting & Searching - Binary Search
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap adjacent elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        // If no swapping occurred, array is already sorted
+        if (!swapped) break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
